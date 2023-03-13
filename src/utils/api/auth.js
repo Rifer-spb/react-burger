e333f1d6bf4fbdfd2e405c2a3b18d1ea4d8c4d7d
Konstantin Marchenko <asik-net@yandex.ru>
2023-03-13 21:13:57 +0300

    sprint-3/step-1

--- src/utils/api/auth.js
+++ src/utils/api/auth.js
@@ -94,7 +94,9 @@ export function resetPasswordRequest(formData) {
     Вот тут в функции getUserRequest я не понял как применить функцию request, так как в ней не отследить коды ответов.
     Поетому создал отдельную функию requestOnly. Через нее и ловлю уже 401 или 403 для обновления токена.
     Так или как? Правально я сделал? Или можно еще как-то по другому?
+    Другой вариант обновления токена я закоментил ниже. Какой вариант лучше?
 */
+
 export async function getUserRequest() {
     const config = {
         method : 'GET',
@@ -103,17 +105,73 @@ export async function getUserRequest() {
             Authorization: 'Bearer ' + getCookie('token')
         }
     };
-    let response = await requestOnly(URL_AUTH_GET_USER,config);
+    let response = await requestOnly(URL_AUTH_GET_USER, config);
     if(!response.ok) {
         if(response.status === 401 || response.status === 403) {
             await refreshTokenRequest();
-            response = await requestOnly(URL_AUTH_REFRESH_TOKEN, config);
+            response = await requestOnly(URL_AUTH_GET_USER, config);
         }
     }
     return checkResponse(response);
 }
 
+/*
+export function getUserRequest() {
+    return requestWithRefreshToken(URL_AUTH_GET_USER,{
+        method : 'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            Authorization: 'Bearer ' + getCookie('token')
+        }
+    });
+}
+*/
 /*****************************************************************************/
+
+export const refreshTokenRequest = () => {
+    return request(URL_AUTH_REFRESH_TOKEN, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+            token : getCookie('refreshToken')
+        }),
+    })
+        .then(res => {
+            setCookie('token', res.accessToken.split('Bearer ')[1]);
+            setCookie('refreshToken', res.refreshToken);
+        }).catch(err => {
+            deleteCookie('token');
+            deleteCookie('refreshToken');
+            window.location.reload();
+        });
+};
+
+/*
+const requestWithRefreshToken = async (url, config) => {
+    return requestOnly(url, config).then(res => {
+        if (!res.ok) {
+            if(res.status === 401 || res.status === 403) {
+                return refreshTokenRequest()
+                    .then(res => res.json())
+                    .then(res => {
+                        if (res.success) {
+                            setCookie('token', res.accessToken.split('Bearer ')[1]);
+                            setCookie('refreshToken', res.refreshToken);
+                            config.headers.Authorization = res.accessToken;
+                            return requestOnly(url, config).then((res) => checkResponse(res))
+                        } else {
+                            deleteCookie('token');
+                            deleteCookie('refreshToken');
+                            window.location.reload();
+                        }
+                    });
+            }
+        }
+        return res.json();
+    });
+};
 
 export const refreshTokenRequest = () => {
     return requestOnly(URL_AUTH_REFRESH_TOKEN, {
@@ -125,19 +183,5 @@ export const refreshTokenRequest = () => {
             token : getCookie('refreshToken')
         }),
     })
-        .then(res => checkResponse(res))
-        .then(res => {
-            if (res.success) {
-                setCookie('token', res.accessToken.split('Bearer ')[1]);
-                setCookie('refreshToken', res.refreshToken);
-            } else {
-                deleteCookie('token');
-                deleteCookie('refreshToken');
-                window.location.reload();
-            }
-        }).catch(err => {
-            deleteCookie('token');
-            deleteCookie('refreshToken');
-            window.location.reload();
-        });
 };
+*/
